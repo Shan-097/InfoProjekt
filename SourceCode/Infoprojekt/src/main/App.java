781ca73f79ec15c7472f5df854aaaab0539ff0cf
notactuallyfinn <mfritzsche3108@gmail.com>
2025-01-15 17:28:36 +0100

    Fixed bug where buttons did not show up und added three buttons

--- SourceCode/Infoprojekt/src/main/App.java
+++ SourceCode/Infoprojekt/src/main/App.java
@@ -7,6 +7,8 @@ import javax.swing.JFrame;
 import game.GamePanel;
 
 public class App {
+    final static int frameRate = 60;
+
     public static void main(String[] args) throws Exception {
         JFrame window = new JFrame("some name");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -14,13 +16,15 @@ public class App {
     }
 
     private static void loadStartingScreen(JFrame window){
-        StartingPanel startingPanel = new StartingPanel();
-        window.add(startingPanel);
+        StartingPanel startingPanel = new StartingPanel(frameRate);
+        
+        window.setContentPane(startingPanel);
         window.pack();
         window.setLocationRelativeTo(null);
         window.setVisible(true);
         window.setExtendedState(JFrame.MAXIMIZED_BOTH);
         window.setMinimumSize(new Dimension(500, 300));//random values, TO DO: choose better
+        startingPanel.startGameThread();
     }
 
     private static void loadGameScreen(JFrame window){
--- SourceCode/Infoprojekt/src/main/StartingPanel.java
+++ SourceCode/Infoprojekt/src/main/StartingPanel.java
@@ -1,27 +1,64 @@
 package main;
 
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-public class StartingPanel extends JPanel {    
-    public StartingPanel(){
+
+public class StartingPanel extends JPanel implements Runnable{   
+    private final int buttonWidth = 100;
+    private final int buttonHeight = 30;
+    private final int frameRate;
+
+    JButton startNewGame;
+    JButton changeHotkeys;
+    JButton loadGame;
+    Thread thread;
+
+    public StartingPanel(int pFR){
+        frameRate = pFR;
+        this.setLayout(null);
         this.setPreferredSize(null);
         this.setDoubleBuffered(true);
-        this.repaint();
+        this.setOpaque(true);
+        startNewGame = new JButton("Create new world");
+        changeHotkeys = new JButton("Change hotkeys");
+        loadGame = new JButton("load existing world");
+        add(startNewGame);
+        add(changeHotkeys);
+        add(loadGame);
     }
 
-    public void paintComponent(Graphics g){
-        super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D)g;
-        JButton startNewGame = new JButton("Create new World");
-        startNewGame.setSize(new Dimension(50, 20));//random values, TO DO: choose better
-        startNewGame.setLocation((this.WIDTH - startNewGame.getWidth()) / 2, (this.HEIGHT - startNewGame.getHeight()) / 2);//random values, TO DO: choose better
-        this.add(startNewGame);
-        g2d.dispose();
+    public void moveButtons(){
+        startNewGame.setBounds((this.getWidth() - buttonWidth) / 2, (this.getHeight() - buttonHeight) / 2 - 40, buttonWidth, buttonHeight);
+        changeHotkeys.setBounds((this.getWidth() - buttonWidth) / 2, (this.getHeight() - buttonHeight) / 2, buttonWidth, buttonHeight);
+        loadGame.setBounds((this.getWidth() - buttonWidth) / 2, (this.getHeight() - buttonHeight) / 2 + 40, buttonWidth, buttonHeight);
+        this.validate();
+        this.setVisible(true);
     }
 
+    public void startGameThread() {
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        double frameDisplayTime = 1000000000 / frameRate;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        double delta = 0;
+
+        while (thread != null) {
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / frameDisplayTime;
+            lastTime = currentTime;
+            if(delta >= 1){
+                moveButtons();
+                delta--;
+            }
+        }
+    }
 }
