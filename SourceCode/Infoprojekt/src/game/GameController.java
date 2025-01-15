fe1cfed4d2e04d434e2c6e4203aa1c5604803a75
notactuallyfinn <mfritzsche3108@gmail.com>
2025-01-15 13:08:02 +0100

    Added a game controller class

---
+++ SourceCode/Infoprojekt/src/game/GameController.java
@@ -0,0 +1,20 @@
+package game;
+
+public class GameController {
+    private int posXinArray;
+    private int posYinArray;
+    private byte posXonTile;//max is 100, min is 0
+    private byte posYonTile;//max is 100, min is 0
+
+
+    public GameController(){
+        posXinArray = 10;
+        posYinArray = 8;
+        posXonTile = 50;
+        posYinArray = 50;
+    }
+
+    public void update(){
+
+    }
+}
