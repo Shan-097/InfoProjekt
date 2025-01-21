e0cfdf679ced5301cb9615f693ccb9ca9e8c4694
notactuallyfinn <mfritzsche3108@gmail.com>
2025-01-21 17:22:15 +0100

    cleaned Resource and Item up and finished work

--- SourceCode/Infoprojekt/src/game/Item.java
+++ SourceCode/Infoprojekt/src/game/Item.java
@@ -3,22 +3,26 @@ package game;
 import java.util.HashMap;
 
 public class Item {
-    private static final HashMap<Integer, String> iDMap = new HashMap<Integer, String>(5);
+    private static final HashMap<Integer, Item> iDMap = new HashMap<Integer, Item>(5);
     private final int itemID;
 
     public static void main(String[] args) {
-        iDMap.put(0, "Brick");
-        iDMap.put(1, "CopperIngot");
-        iDMap.put(2, "IronIngot");
-        iDMap.put(3, "GoldIngot");
-        iDMap.put(4, "Stone");
-        iDMap.put(5, "CopperOre");
-        iDMap.put(6, "IronOre");
-        iDMap.put(7, "GoldOre");
+        iDMap.put(0, new Item(0)); //brick
+        iDMap.put(1, new Item(1)); //copper ingot
+        iDMap.put(2, new Item(2)); //iron ingot
+        iDMap.put(3, new Item(3)); //gold ingot
+        iDMap.put(4, new Item(4)); //stone
+        iDMap.put(5, new Item(5)); //copper ore
+        iDMap.put(6, new Item(6)); //iron ore
+        iDMap.put(7, new Item(7)); //gold ore
     }
 
-    public Item(int pItemID){
+    private Item(int pItemID){
         itemID = pItemID;
+    }
+
+    public static Item getItemWithID(int pItemID){
+        return iDMap.get(pItemID);
     }
 
     public int getItemID(){
--- SourceCode/Infoprojekt/src/game/Resource.java
+++ SourceCode/Infoprojekt/src/game/Resource.java
@@ -3,19 +3,23 @@ package game;
 import java.util.HashMap;
 
 public class Resource {
-    private static final HashMap<Integer, String> iDMap = new HashMap<Integer, String>(5);
+    private static final HashMap<Integer, Resource> iDMap = new HashMap<Integer, Resource>(5);
     private final int resourceID;
 
     public static void main(String[] args) {
-        iDMap.put(0, "NoRessource");
-        iDMap.put(1, "Stone");
-        iDMap.put(2, "Copper");
-        iDMap.put(3, "Iron");
-        iDMap.put(4, "Gold");
+        iDMap.put(0, new Resource(0)); //no resource
+        iDMap.put(1, new Resource(1)); //stone
+        iDMap.put(2, new Resource(1)); //copper
+        iDMap.put(3, new Resource(1)); //iron
+        iDMap.put(4, new Resource(1)); //gold
     }
 
-    public Resource(int pResourceID){
+    private Resource(int pResourceID){
         resourceID = pResourceID;
+    }
+
+    public static Resource getResourceWithID(int pResourceID){
+        return iDMap.get(pResourceID);
     }
 
     public int getResourceID(){
