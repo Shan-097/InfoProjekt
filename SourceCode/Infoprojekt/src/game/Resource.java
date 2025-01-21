57a732ba457ddbfbd6b21e4aef66653bae105fdf
notactuallyfinn <mfritzsche3108@gmail.com>
2025-01-21 10:15:07 +0100

    Reworked Resources

--- SourceCode/Infoprojekt/src/game/Resource.java
+++ SourceCode/Infoprojekt/src/game/Resource.java
@@ -1,5 +1,24 @@
 package game;
 
-public abstract class Resource {
-    public abstract int getResourceID();
+import java.util.HashMap;
+
+public class Resource {
+    private static final HashMap<Integer, String> iDMap = new HashMap<Integer, String>(5);
+    private final int resourceID;
+
+    public static void main(String[] args) {
+        iDMap.put(0, "NoRessource");
+        iDMap.put(1, "Stone");
+        iDMap.put(2, "Copper");
+        iDMap.put(3, "Iron");
+        iDMap.put(4, "Gold");
+    }
+
+    public Resource(int pResourceID){
+        resourceID = pResourceID;
+    }
+
+    public int getResourceID(){
+        return resourceID;
+    }
 }
