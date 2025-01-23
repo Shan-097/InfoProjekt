f93919ab9206e136b162902765df752a60b37dd6
notactuallyfinn <mfritzsche3108@gmail.com>
2025-01-23 11:22:12 +0100

    finished work on world generation

--- SourceCode/Infoprojekt/src/game/WorldGenerator.java
+++ SourceCode/Infoprojekt/src/game/WorldGenerator.java
@@ -1,14 +1,17 @@
 package game;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Random;
-import java.lang.Math;
 
 /**
  * to be done
  */
 public class WorldGenerator {
+    private static final double baseProbRes0 = 0.95d;
+    private static final double baseProbRes1 = 0.02d;
+    private static final double baseProbRes2 = 0.015d;
+    private static final double baseProbRes3 = 0.01d;
+    private static final double baseProbRes4 = 0.005d;
     private Field[][] map;
 
     /**
@@ -31,7 +34,7 @@ public class WorldGenerator {
         if (map[posX][posY] != null) {
             return;
         }
-        chooseTileToBeginFuctionCollapse(posX, posY);
+        chooseResource(posX, posY);
         if (posY != 0) {
             collapseFunctionWave(posX, posY - 1);
         }
@@ -49,36 +52,23 @@ public class WorldGenerator {
     /**
      * to be done
      * 
-     * @param ressources to be done
+     * @param probability to be done
      * @return to be done
      */
-    private int generateRandomTileWeighted(int[] ressources) {
-        byte countRes1 = 0, countRes2 = 0, countRes3 = 0, countRes4 = 0;
-        for (byte i = 0; i < ressources.length; i++) {
-            if (ressources[i] == 1) {
-                countRes1++;
-            } else if (ressources[i] == 2) {
-                countRes2++;
-            } else if (ressources[i] == 3) {
-                countRes3++;
-            } else if (ressources[i] == 4) {
-                countRes4++;
-            }
-        }
-        Random rnd = new Random();
-        double rand = rnd.nextDouble(1011 + (Math.pow(countRes1, 0.2) + Math.pow(countRes2, 0.2)
-                + Math.pow(countRes3, 0.2) + Math.pow(countRes4, 0.2)) * 600);
-        if (rand < 1000) {
+    private int generateRandomResource(double[] probability) {
+        // assumes length 5
+        double rnd = new Random().nextDouble();
+        if (rnd < probability[0]) {
             return 0;
-        } else if (rand < (1003.5 + Math.pow(countRes1, 0.2) * 600)) {
+        } else if (rnd < probability[0] + probability[1]) {
             return 1;
-        } else if (rand < (1006.5 + (Math.pow(countRes1, 0.2) + Math.pow(countRes2, 0.2)) * 600)) {
+        } else if (rnd < probability[0] + probability[1] + probability[2]) {
             return 2;
-        } else if (rand < (1009
-                + (Math.pow(countRes1, 0.2) + Math.pow(countRes2, 0.2) + Math.pow(countRes4, 0.2)) * 600)) {
+        } else if (rnd < probability[0] + probability[1] + probability[2] + probability[3]) {
             return 3;
+        } else {
+            return 4;
         }
-        return 4;
     }
 
     /**
@@ -87,8 +77,118 @@ public class WorldGenerator {
      * @param posX to be done
      * @param posY to be done
      */
-    private void chooseTileToBeginFuctionCollapse(int posX, int posY) {
-        // still to be implemented
+    private void chooseResource(int posX, int posY) {
+        if (map[posX][posY] != null) {
+            return;
+        }
+        // it is known that at max one tile can have
+        // a resource on it that isn't noResource
+        int iDofKnownResource = -1;
+        int countOfKnownTiles = 0;
+        if (posY != 0 && map[posX][posY - 1] != null) {
+            countOfKnownTiles++;
+            if (map[posX][posY - 1].getResourceID() != 0) {
+                iDofKnownResource = map[posX][posY - 1].getResourceID();
+            }
+        } else if (posY != map[0].length - 1 && map[posX][posY + 1] != null) {
+            countOfKnownTiles++;
+            if (map[posX][posY + 1].getResourceID() != 0) {
+                iDofKnownResource = map[posX][posY + 1].getResourceID();
+            }
+        }
+        if (posX != 0 && map[posX - 1][posY] != null) {
+            countOfKnownTiles++;
+            if (map[posX - 1][posY].getResourceID() != 0) {
+                iDofKnownResource = map[posX - 1][posY].getResourceID();
+            }
+        } else if (posX != map.length - 1 && map[posX + 1][posY] != null) {
+            countOfKnownTiles++;
+            if (map[posX + 1][posY].getResourceID() != 0) {
+                iDofKnownResource = map[posX + 1][posY].getResourceID();
+            }
+        }
+        double[] probability = new double[5];
+        if (iDofKnownResource == -1) {
+            switch (countOfKnownTiles) {
+                case 0:
+                    probability[0] = baseProbRes0;
+                    probability[1] = baseProbRes1;
+                    probability[2] = baseProbRes2;
+                    probability[3] = baseProbRes3;
+                    probability[4] = baseProbRes4;
+                    break;
+
+                case 1:
+                    probability[0] = 1 - (1 - baseProbRes0) / 2;
+                    probability[1] = baseProbRes1 / 2;
+                    probability[2] = baseProbRes2 / 2;
+                    probability[3] = baseProbRes3 / 2;
+                    probability[4] = baseProbRes4 / 2;
+                    break;
+
+                case 2:
+                    probability[0] = 1 - (1 - baseProbRes0) / 4;
+                    probability[1] = baseProbRes1 / 4;
+                    probability[2] = baseProbRes2 / 4;
+                    probability[3] = baseProbRes3 / 4;
+                    probability[4] = baseProbRes4 / 4;
+                    break;
+
+                case 3:
+                    probability[0] = 1 - (1 - baseProbRes0) / 8;
+                    probability[1] = baseProbRes1 / 8;
+                    probability[2] = baseProbRes2 / 8;
+                    probability[3] = baseProbRes3 / 8;
+                    probability[4] = baseProbRes4 / 8;
+                    break;
+
+                case 4:
+                    probability[0] = 1;
+                    break;
+            }
+        } else {
+            double baseProbFoundRes = 0;
+            switch (iDofKnownResource) {
+                case 1:
+                    baseProbFoundRes = baseProbRes1;
+                    break;
+
+                case 2:
+                    baseProbFoundRes = baseProbRes2;
+                    break;
+
+                case 3:
+                    baseProbFoundRes = baseProbRes3;
+                    break;
+
+                case 4:
+                    baseProbFoundRes = baseProbRes4;
+                    break;
+            }
+
+            switch (countOfKnownTiles - 1) {
+                case 0:
+                    probability[0] = 1 - 10 * baseProbFoundRes;
+                    probability[iDofKnownResource] = 10 * baseProbFoundRes;
+                    break;
+
+                case 1:
+                    probability[0] = 1 - 6 * baseProbFoundRes;
+                    probability[iDofKnownResource] = 8 * baseProbFoundRes;
+                    break;
+
+                case 2:
+                    probability[0] = 1 - 3 * baseProbFoundRes;
+                    probability[iDofKnownResource] = 3 * baseProbFoundRes;
+                    break;
+
+                case 3:
+                    probability[0] = 1 - baseProbFoundRes;
+                    probability[iDofKnownResource] = baseProbFoundRes;
+                    break;
+            }
+        }
+        map[posX][posY] = new Field(generateRandomResource(probability));
     }
 
     /**
