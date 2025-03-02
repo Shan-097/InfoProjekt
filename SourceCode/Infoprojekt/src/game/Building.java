4c28d5320daa9707e4c91df12c4aa1ca90743920
notactuallyfinn <mfritzsche3108@gmail.com>
2025-03-02 19:35:57 +0100

    Commented two classes

--- SourceCode/Infoprojekt/src/game/Building.java
+++ SourceCode/Infoprojekt/src/game/Building.java
@@ -4,21 +4,27 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 /**
- * Abstract class Building defines the necessary methods
+ * Abstract class Building defines the necessary methods and variables of a
+ * building.<br>
+ * The method limiting a behaviors of a subclass should be overwritten by it.
  */
 public abstract class Building {
     /**
-     * to be done
+     * The rotation of the building.<br>
+     * Rotation is in {0, 1, 2, 3}.
      */
     private byte rotation;
 
     /**
-     * to be done
+     * The inventory of the building.<br>
+     * Always has five items in it (if necessary null). Items are moved one spot
+     * each time the items are moved.
      */
     private LinkedList<Item> content;
 
     /**
-     * to be done
+     * Basic constructor of Building for super() calls.<br>
+     * Initalizes the rotation and inventory of the building.
      */
     public Building() {
         rotation = 0;
@@ -26,9 +32,14 @@ public abstract class Building {
     }
 
     /**
-     * to be done
+     * Adds an item to the inventory.<br>
+     * Returns whether or not the operation was sucessful. The operation is defined
+     * as successfull, if and only if the last item in the inventory is null or the
+     * one to be added is null.
      * 
-     * @param item to be done
+     * @param item The item to be added to the inventory.
+     * @return True if and only if the last item in the inventory or the one to be
+     *         added is null. And false otherwise.
      */
     public boolean addItem(Item item) {
         if (item == null) {
@@ -42,9 +53,14 @@ public abstract class Building {
     }
 
     /**
-     * to be done
+     * Moves the first item of this building to the given building.<br>
+     * Additionally executes the building specific method on the first item in the
+     * inventory before trying to move it to the next building.<br>
+     * Tries to add the modified first item to the next building. If this is
+     * successful finally remove the item from this buildings inventory and fill it
+     * up with null.
      * 
-     * @param otherBuilding to be done
+     * @param otherBuilding The building the item should be transferred to.
      */
     public void moveItemToNextBuilding(Building otherBuilding) {
         content.addFirst(executeFunction(content.pollFirst()));
@@ -55,54 +71,72 @@ public abstract class Building {
     }
 
     /**
-     * to be done
+     * Rotates the builing by adding 1 to the rotation and making sure that the
+     * result is valid.
      */
     public void rotate() {
         this.setRotation((byte) ((getRotation() + 1) % 4));
     }
 
     /**
-     * to be done
+     * Returns the rotation of this building.
      * 
-     * @return byte to be done
+     * @return The rotation.
      */
     public byte getRotation() {
         return rotation;
     }
 
     /**
-     * to be done
+     * The building specific method. Has to be implemented by subclasses to specifiy
+     * the behavior of this building on items passed through it.<br>
+     * Takes one item and returns the processed variant.
      * 
-     * @param item to be done
-     * @return Item to be done
+     * @param item The item to be modified.
+     * @return The modified item.
      */
     protected abstract Item executeFunction(Item item);
 
     /**
-     * to be done
+     * Returns the cost of the building.<br>
+     * It has to be abstract because the cost of a building is expected to be final
+     * and static. It therefor can't be specified in Building. So that this method
+     * can't be implemented in Building.
      * 
-     * @return to be done
+     * @return The cost of the building.
      */
     public abstract HashMap<Item, Integer> getCost();
 
     /**
-     * to be done
+     * Returns the input directions of the building.<br>
+     * It has to be abstract because the input directions of a building are expected
+     * to be at least static. It therefor can't be specified in Building. So that
+     * this method can't be implemented in Building.
      * 
-     * @return to be done
+     * @return The input directions of this building. It may be 0, 1, 2, 3 or 4
+     *         values.
      */
     public abstract byte[] getInputDirections();
 
     /**
-     * to be done
+     * Returns the output directions of the building.<br>
+     * It has to be abstract because the output directions of a building are
+     * expected
+     * to be static in most cases. It therefor can't be specified in Building. So
+     * that
+     * this method can't be implemented in Building.
      * 
-     * @return to be done
+     * @return The output directions of this building. It may be 0, 1, 2, 3 or 4
+     *         values.
      */
     public abstract byte[] getOutputDirections();
 
     /**
-     * to be done
+     * The set method for rotation.<br>
+     * It should only be used inside of subclasses from Building to set their own
+     * rotation. Otherwise unexpected behavior may occure.
      * 
-     * @param pRotation to be done
+     * @param pRotation The new rotation.
      */
     public void setRotation(byte pRotation) {
         rotation = pRotation;
